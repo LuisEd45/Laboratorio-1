d881018c06d00bbe45f0eaeeeac4f6ed90cb5442
LuisEd45 <99060584+LuisEd45@users.noreply.github.com>
2022-02-04 21:33:47 -0600

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratorio_1
+{
+    public partial class Form1 : Form
+    {
+        Persona a = new Persona();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label11_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a.Dpi = textBoxDpi.Text;
+            a.Nombre = textBoxNombre.Text;
+            a.Apellido = textBoxApellido.Text;
+            a.FechaNacimiento = dateTimePicker1.Value;
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            labelDpi.Text = a.Dpi;
+            labelNombre.Text = a.Nombre;
+            labelApellido.Text = a.Apellido;
+            labelFechaNacimiento.Text = a.FechaNacimiento.ToString();
+            labelEdad.Text = a.edad().ToString();
+        }
+        public void normalizaNombre()
+        {
+            string nombre2 = "";
+            a.Nombre = a.Nombre.Trim();
+            nombre2 += char.ToUpper(a.Nombre[0]);
+            for (int x = 1; x < a.Nombre.Length; x++)
+            {
+                if (a.Nombre[x - 1] == ' ')
+                {
+                    if (char.IsLetter(a.Nombre[x]) || char.IsWhiteSpace(a.Nombre[x]))
+                        nombre2 += char.ToUpper(a.Nombre[x]);
+                }
+                else
+                 if (char.IsLetter(a.Nombre[x]) || char.IsWhiteSpace(a.Nombre[x]))
+                    nombre2 += a.Nombre[x];
+
+            }
+        }
+    }
+}
---
+++ Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Laboratorio_1
+{
+    internal class Persona
+    {
+        public string Dpi { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime FechaNacimiento { get; set; }
+
+        public int edad()
+        {
+            DateTime hoy = DateTime.Today;
+            int edad = hoy.Year - FechaNacimiento.Year;
+            if (FechaNacimiento.Month > hoy.Month)
+            {
+                --edad;
+            }
+            return edad;
+
+        }
+        
+}
+
+}
